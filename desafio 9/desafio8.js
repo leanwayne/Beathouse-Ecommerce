63c1d02928b04bba76a6b4eb7637915555adc9bc
Lean <lean_rap50rhotmail.com>
2021-03-19 17:23:22 -0300

    desafio 09

--- desafio 9/desafio8.js
+++ desafio 9/desafio8.js
@@ -1,35 +1,68 @@
 const express = require('express')
 const multer = require ("multer") 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/static',express.static('static'))
-
 const router = express.Router()
-app.use('/api', router)
-let listaProductos = [];
-
-
 
 const isProduct = (object)=>{
   return object.title && typeof object.title === "string"  
   && object.price && typeof object.price === "number" 
-  && object.title && typeof object.title === "string";
+  && object.thumbnail;
 }
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '')
+  }, 
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+let upload = multer({ storage: storage })
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+let product = {
+  title: '',
+  price: '',
+  thumbnail: ''
+};
+let listaProductos = [];
 
-router.get('/',(req, res) =>{
-  res.sendFile('/index.html')
+//form
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 })
 
-router.post("/productos", (req, res) => {
-  if(!isProduct(req.body)) {
+
+
+
+
+//guardar product en el array
+router.post("/productos", upload.single('thumbnail'), (req, res, next) => {
+  const file = req.file
+  if(!file) {
+      const error = new Error('Please upload a file')
+      error.httpStatusCode = 400
+      return next(error)
+  }
+  if(!req.body.title && !req.body.price && !req.body.thumbnail) {
     return res.status(400).send("Error en los parametros");
   }
-  req.body.id = (listaProductos.length + 1).toString();
-  listaProductos.push(req.body);
+  product = { 
+    title: req.body.title,
+    price: req.body.price,
+    thumbnail: file.originalname,
+    id: (listaProductos.length + 1).toString()
+  };
+  listaProductos.push(product);
   return res.status(200).send(listaProductos);
 });
 
+
+
+
+
+
+
+//get lista de productos entera
 router.get("/productos", (req, res) => {
   if (listaProductos.length === 0) {
     return res.status(404).send({ error: "no hay productos cargados" });
@@ -37,6 +70,9 @@ router.get("/productos", (req, res) => {
   res.status(200).send(listaProductos);
 });
 
+
+
+//get producto por id
 router.get("/productos/:id", (req, res) => {
   const producto = listaProductos.find(
     (product) => product.id === req.params.id
@@ -47,6 +83,12 @@ router.get("/productos/:id", (req, res) => {
   return res.status(200).json(producto);
 });
 
+
+
+
+
+
+//cambiar producto por id
 router.put("/productos/:id", (req, res) => {
   if(!isProduct(req.body)) {
     return res.status(400).send("Error en los parametros");
@@ -61,7 +103,7 @@ router.put("/productos/:id", (req, res) => {
   listaProductos = arrActualizado
   return res.status(200).send(listaProductos);
 });
-
+//borra producto por id
 router.delete("/productos/:id", (req, res) => {
   const producto = listaProductos.find(product => product.id === req.params.id)
   if (!producto) {
@@ -72,6 +114,9 @@ router.delete("/productos/:id", (req, res) => {
   return res.status(200).send(listaProductos);
 });
 
+
+
+app.use('/api', router)
 const server = app.listen(8080, () => { 
   console.log("Running");
 });
