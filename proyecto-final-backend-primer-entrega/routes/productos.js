c2c74d4b0aa3dfc2569ae28f37156e242e5ffbd3
Lean <lean_rap50rhotmail.com>
2021-04-16 13:09:00 -0300

    primer entrega

--- proyecto-final-backend-primer-entrega/routes/productos.js
+++ proyecto-final-backend-primer-entrega/routes/productos.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-let moment = require('moment')
-const fs = require('fs');
-const contenido = fs.readFileSync('productos.txt', 'utf-8');
-let listaProductos = (JSON.parse(contenido))
+let moment = require("moment");
+const fs = require("fs");
+const contenido = fs.readFileSync("productos.txt", "utf-8");
+let listaProductos = JSON.parse(contenido);
 const isProduct = (object) => {
   return (
     object.title &&
@@ -14,36 +14,32 @@ const isProduct = (object) => {
     typeof object.thumbnail === "string"
   );
 };
-var administrador = true
-
+var administrador = true;
 
 //guardar product en el array---------------------------------------------------------------------------------------------------------------------
 router.post("/agregar", (req, res) => {
   if (!req.body.nombre && !req.body.precio && !req.body.foto) {
     return res.status(400).send("Error en los parametros");
   }
-   let product = {
+  let product = {
     nombre: req.body.nombre,
     precio: req.body.precio,
     descripcion: req.body.descripcion || "sin descripcion",
-    codigo: moment().format('DD/MM/YYYY HH:MM:SS'),
+    codigo: moment().format("DD/MM/YYYY HH:MM:SS"),
     foto: req.body.foto,
     stock: 20,
-    timestamp: moment().format('DD/MM/YYYY HH:MM:SS'),
+    timestamp: moment().format("DD/MM/YYYY HH:MM:SS"),
     id: (listaProductos.length + 1).toString(),
   };
-  listaProductos.push(product);  
-  let file = (JSON.stringify(listaProductos))
-  fs.writeFileSync('./productos.txt', file) 
-  return res
-    .status(200)
-    .json(listaProductos);
+  listaProductos.push(product);
+  let file = JSON.stringify(listaProductos);
+  fs.writeFileSync("./productos.txt", file);
+  return res.status(200).json(listaProductos);
 });
 
 //get lista de productos entera--------------------------------------------------------------------------------------------------------------------
 router.get("/listar", (req, res) => {
-  res.status(200)
-  .json(listaProductos);
+  res.status(200).json(listaProductos);
 });
 
 //get producto por id---------------------------------------------------------------------------------------------------------------------------
@@ -58,54 +54,51 @@ router.get("/listar/:id", (req, res) => {
 });
 
 //cambiar producto por id--------------------------------------------------------------------------------------------------------------------------
-router.put("/actualizar/:id", verificar,(req, res) => {
-    if (!isProduct(req.body)) {
-      return res.status(400).send("Error en los parametros");
-    }
-    const producto = listaProductos.find(
-      (product) => product.id === req.params.id
-    );
-    if (!producto) {
-      return res.status(400).send("No existe un producto con ese id");
-    }
-    const arrActualizado = listaProductos.filter(
-      (product) => product.id !== producto.id
-    );
-    req.body.id = producto.id;
-    arrActualizado.push(req.body);
-    listaProductos = arrActualizado;
-    let file = (JSON.stringify(listaProductos))
-    fs.writeFileSync('./productos.txt', file)  
+router.put("/actualizar/:id", verificar, (req, res) => {
+  if (!isProduct(req.body)) {
+    return res.status(400).send("Error en los parametros");
+  }
+  const producto = listaProductos.find(
+    (product) => product.id === req.params.id
+  );
+  if (!producto) {
+    return res.status(400).send("No existe un producto con ese id");
+  }
+  const arrActualizado = listaProductos.filter(
+    (product) => product.id !== producto.id
+  );
+  req.body.id = producto.id;
+  arrActualizado.push(req.body);
+  listaProductos = arrActualizado;
+  let file = JSON.stringify(listaProductos);
+  fs.writeFileSync("./productos.txt", file);
 
-    return res.status(200).json(listaProductos);
-  });
+  return res.status(200).json(listaProductos);
+});
 
 //borra producto por id-------------------------------------------------------------------------------------------------------------------------
 router.delete("/borrar/:id", verificar, (req, res) => {
-    const producto = listaProductos.find(
-      (product) => product.id === req.params.id
-    );
-    if (!producto) {
-      return res.status(400).send("No existe un producto con ese id");
-    }
-    let arrActualizado = listaProductos.filter(
-      (product) => product.id !== producto.id
-    );
-    listaProductos = arrActualizado;
-    let file = (JSON.stringify(listaProductos))
-    fs.writeFileSync('./productos.txt', file)
-    return res.status(200).send(listaProductos);
-  });
-
-
-  function verificar(req ,res ,next){
-    if(!administrador){
-      res.json({error:"Acceso denegado, usted no es un administrador"})
-    }
-    else{
-      next()
-    }
+  const producto = listaProductos.find(
+    (product) => product.id === req.params.id
+  );
+  if (!producto) {
+    return res.status(400).send("No existe un producto con ese id");
   }
+  let arrActualizado = listaProductos.filter(
+    (product) => product.id !== producto.id
+  );
+  listaProductos = arrActualizado;
+  let file = JSON.stringify(listaProductos);
+  fs.writeFileSync("./productos.txt", file);
+  return res.status(200).send(listaProductos);
+});
 
+function verificar(req, res, next) {
+  if (!administrador) {
+    res.json({ error: "Acceso denegado, usted no es un administrador" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
